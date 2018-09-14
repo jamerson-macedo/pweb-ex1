e0fb364e900f09d95c10cb653c54d478be238478
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2018-09-13 23:11:56 -0300

    Pagina quase pronta

--- src/main/java/br/ufrn/ceres/bsi/pweb/Servlet.java
+++ src/main/java/br/ufrn/ceres/bsi/pweb/Servlet.java
@@ -10,16 +10,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class servlet
  */
-@WebServlet("questao3")
-
+@WebServlet("/questao3")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -38,7 +33,8 @@ public class Servlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		handleRequest(request, response);
 	}
 
 	/**
@@ -46,7 +42,7 @@ public class Servlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		handleRequest(request, response);
 	}
 
 		public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -19,15 +19,14 @@
 	<a href="jamerson macedo"> Jamerson macedo</a>
 	<br>
 	<a href="jamerson_macedo@hotmail.com"> Meu E-mail</a>
-	<h2>
-
+	<br>
 	<a href="jamerson/q1-cgi.html">Questão 1</a>
 	<br>
 	<a href="jamerson/q2-servidores.html">Questão 2</a>
 	<br>
-	<a href= "questao3">Questão 3 </a>
+	<a href= "questao3">Questão 3</a>
+	<br>
+	<a href="jamerson/curriculo.html">Meu Curriculo - Questão 4 </a>
 	
-	
-	</h2>
 </body>
 </html>
